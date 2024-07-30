36371eebd65e3e2c864bd1f016122b5a2dd15cac
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-30 14:53:57 +0200

    feat: add presentation class

---
+++ presentation.py
@@ -0,0 +1,52 @@
+import copy
+import pandas
+import matplotlib.pyplot as plt
+
+from typing import Optional, Tuple
+from settings import Path, Columns
+from data import DataManager
+
+
+class Presentation():
+    paths = Path
+    columns = Columns
+
+    manager: DataManager
+
+    __dataframe: Optional[pandas.DataFrame] = None
+
+    title: str = 'Apple Stock Close Price'
+    context: str = 'Close Price'
+    xlabel: str = 'Date'
+    ylabel: str = 'Close Price (USD)'
+    size: Tuple[int, int] = (10, 6)
+
+    def __init__(self) -> None:
+        self.manager = DataManager()
+
+    def visualize(self) -> None:
+        plt.figure(figsize=self.size)
+        plt.plot(self.dataframe[self.columns.close.value], label=self.context)
+        plt.title(self.title)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    @property
+    def dataframe(self) -> pandas.DataFrame:
+        if self.__dataframe is not None:
+            return self.__dataframe
+
+        self.__dataframe = self.clean(self.manager.dataframe)
+        return self.__dataframe
+
+    def clean(self, dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        df = copy.deepcopy(dataframe)
+
+        df[self.columns.date.value] = pandas.to_datetime(
+            df[self.columns.date.value])
+        df.set_index(self.columns.date.value, inplace=True)
+
+        return df

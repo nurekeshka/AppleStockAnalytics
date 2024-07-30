ab7e3644d44d373bd1d24910a67c220865ca0a09
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-30 15:37:48 +0200

    fix: move application entry point to main function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from typing import Optional
+from typing import Literal, Callable, cast
 import sys
 
 from presentation import Presentation
 from data import DataManager
 
 
-def present():
+def present(type: Literal['matplotlib', 'plotly']):
     presentation = Presentation()
-    presentation.visualize()
+
+    try:
+        visualization: Callable = (getattr(presentation, type))
+    except AttributeError:
+        print('Such a visualization doesn\'t exist.')
+
+    visualization()
 
 
 def setup():
@@ -15,13 +21,23 @@ def setup():
     manager.setup()
 
 
-if __name__ == '__main__':
-    cmd: Optional[str] = sys.argv[1] if len(sys.argv) > 1 else None
+def main():
+    task = sys.argv[1] if len(sys.argv) > 1 else ''
 
-    match cmd:
+    match task:
         case 'present':
-            present()
+            presentation = sys.argv[2] if len(sys.argv) > 2 else ''
+
+            if presentation not in Presentation.options:
+                print('Such a presentation doesn\'t exist.')
+                return
+
+            present(cast(Literal['matplotlib', 'plotly'], presentation))
         case 'setup':
             setup()
         case _:
             print('Such a command doesn\'t exist.')
+
+
+if __name__ == '__main__':
+    main()
